cfbc8aed265c02a31f94c37ed1183bb7e349f861
ESIDEM <solutionz18@ymail.com>
2018-10-30 21:51:43 +0100

    Feat: First Launch Check added

--- app/src/main/java/ng/com/techdepo/esidemtest/SplashScreen.java
+++ app/src/main/java/ng/com/techdepo/esidemtest/SplashScreen.java
@@ -1,6 +1,7 @@
 package ng.com.techdepo.esidemtest;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ public class SplashScreen extends AppCompatActivity {
 
     // Splash Screen Timer
     private static int SPLASH_TIME_OUT = 3000;
+    SharedPreferences prefs = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +21,36 @@ public class SplashScreen extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+        prefs = getSharedPreferences("ng.com.techdepo.esidemtest", MODE_PRIVATE);
 
         new Handler().postDelayed (new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent (SplashScreen.this, MainActivity.class);
-                startActivity(i);
+
+
+                checkFirstRun();
 
                 finish ();
             }
         }, SPLASH_TIME_OUT);
+
+
+    }
+
+    private void checkFirstRun(){
+
+        if (prefs.getBoolean("firstrun", true)) {
+            // Do first run stuff here then set 'firstrun' as false
+            startActivity(new Intent(getApplicationContext(),DataActivity.class));
+            // using the following line to edit/commit prefs
+
+        }else {
+
+            Intent i = new Intent (SplashScreen.this, MainActivity.class);
+            startActivity(i);
+        }
+
+        prefs.edit().putBoolean("firstrun", false).commit();
+
     }
 }
