d8103a933d639d4a9b9dddd002ac603098dc8760
ESIDEM <solutionz18@ymail.com>
2018-10-30 20:25:57 +0100

    Feat: SplashScreen Added

---
+++ app/src/main/java/ng/com/techdepo/esidemtest/SplashScreen.java
@@ -0,0 +1,33 @@
+package ng.com.techdepo.esidemtest;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    // Splash Screen Timer
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed (new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent (SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                finish ();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
